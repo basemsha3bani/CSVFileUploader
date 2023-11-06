e8707904a4ec74c38dc9429c685ae8f0a5968ffe
basemsha3bani <79589623+basemsha3bani@users.noreply.github.com>
2023-11-06 22:46:04 +0300

    Add files via upload

---
+++ CSVFileUploader/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSVFileUploader.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index(MenuDisplayViewModel menuDisplayModel)
+        {
+            return View(menuDisplayModel);
+        }
+
+        [HttpPost]
+        public IActionResult Search(MenuDisplayViewModel searchModel)
+        {
+            /*For simplicity this is a dummy list for test*/
+            List<MenuItemViewModel> menuItems = new List<MenuItemViewModel>
+        {
+           new MenuItemViewModel{Action="Index",Controller="Teacher",LinkText="TeacherController",userType=UserType.Teacher},
+           new MenuItemViewModel{Action="Index",Controller="Student",LinkText="StudentController",userType=UserType.Student},
+        };
+
+            searchModel.menuItems = menuItems.Where(w => w.userType== searchModel.userType).ToList();
+
+            return View("Index", searchModel);
+        }
+
+    }
+}
